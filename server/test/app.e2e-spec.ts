e1e2d4544771462a22fcd7ff446d7ef7a4d27649
macpli <pliszekmaciej0@gmail.com>
2025-02-02 00:14:42 +0100

    feat: tests for GET endpoint for empty and mocked data array

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,25 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { AppService } from './../src/app.service';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
+  interface Superhero {
+    name: string;
+    superpower: string;
+    humilityScore: number;
+  }
+  
+  let appService = { 
+    getSuperheroes: (): Superhero[] => [], 
+    addSuperhero: jest.fn() 
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+    .overrideProvider(AppService)
+    .useValue(appService)
+    .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/superheroes (GET)', () => {
+    appService.getSuperheroes = () => [];
     return request(app.getHttpServer())
-      .get('/')
+      .get('/superheroes')
       .expect(200)
-      .expect('Hello World!');
+      .expect([]);
+  });
+
+  it('/superheroes (GET) with mock data', () => {
+    appService.getSuperheroes = () => [
+      { name: 'Batman', superpower: 'Martial Arts', humilityScore: 4 },
+      { name: 'Superman', superpower: 'Super Strength', humilityScore: 8 }
+    ];
+    return request(app.getHttpServer())
+      .get('/superheroes')
+      .expect(200)
+      .expect(appService.getSuperheroes());
   });
 });
